c83fec9adbcd48596164644d553f9d5542d4e26f
Max Xu <max.xu@biqmind.com>
2019-07-26 23:30:11 +0800

    update the reducers next: build the middleware

---
+++ src/reducers/decks.js
@@ -0,0 +1,43 @@
+import { ADD_NEW_CARD, ADD_NEW_DECK, RECEIVE_DECKS } from "../actions/decks";
+
+const initialState = {
+  AnnaKarenina: {
+    title: "AnnaKarenina",
+    qss: [
+      {
+        qs: "What is the heroine of the book?",
+        ans: "Anna Karenina"
+      },
+      {
+        qs: "What is the name of Anna Karenina's husband?",
+        ans: "Alexei Alexandrovich Karenin"
+      }
+    ]
+  }
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_DECKS:
+      return {
+        ...state,
+        ...action.payload
+      };
+    case ADD_NEW_DECK:
+      return {
+        ...state,
+        ...action.payload
+      };
+    case ADD_NEW_CARD:
+      const { decKey, card } = action.payload;
+      return {
+        ...state,
+        [decKey]: {
+          ...state[decKey],
+          qss: state[decKey].qss.concat(card)
+        }
+      };
+    default:
+      return state;
+  }
+}
